bb79c0ff4579bc8b0d3bfc4cf865e4c784d7eee6
Aqshola <aafid0127@pnm.co.id>
2025-10-20 16:22:20 +0700

    feat: add layout and reset css

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,7 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import GlobalAppWrapper from "./components/wrapper/GlobalAppWrapper.tsx";
 import "@ant-design/v5-patch-for-react-19";
-
+import "./styles/reset.css";
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <GlobalAppWrapper />
