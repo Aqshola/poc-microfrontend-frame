3f42c23d82f38d902086a17bd4714af226c7400d
Aqshola <aafid0127@pnm.co.id>
2025-10-20 16:31:26 +0700

    feat: init base with layout

--- src/routes/route.ts
+++ src/routes/route.ts
@@ -2,6 +2,7 @@ import { createRoute, createRouter } from "@tanstack/react-router";
 import rootRoute from "./root";
 import Login from "../pages/auth/Login";
 import Base from "../pages/base/Base";
+import PrivateLayout from "../components/layout/PrivateLayout";
 
 const loginRoute = createRoute({
   getParentRoute: () => rootRoute,
@@ -12,8 +13,17 @@ const loginRoute = createRoute({
 const baseRoute = createRoute({
   getParentRoute: () => rootRoute,
   path: "/",
+  component: PrivateLayout,
+});
+
+const baseChild = createRoute({
+  getParentRoute: () => baseRoute,
+  path: "base",
   component: Base,
 });
+
+baseRoute.addChildren([baseChild]);
+
 const routeTree = rootRoute.addChildren([baseRoute, loginRoute]);
 
 const router = createRouter({
